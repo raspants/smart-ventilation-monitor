e1e5541a63e048da8cfbfc8c2f9c5b7649421371
Daniela <danielaloow@outlook.com>
2026-09-03 11:47:50 +0200

    feat: update database schema for ventilation measurements

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,3 @@
--- Jag har lagt en grund så vi har något att utgå ifrån. 
--- Vi justerar schemat när vi spikat payload/API.
--- Stort auto-ID som primärnyckel, device-ID som måste finnas och vara unikt, 
--- och timestamp som automatiskt sätts till nu.
-
-
 CREATE TABLE devices (
     id SERIAL PRIMARY KEY,
     device_id VARCHAR(100) UNIQUE NOT NULL,
@@ -15,44 +9,26 @@ CREATE TABLE devices (
 CREATE TABLE measurements (
     id BIGSERIAL PRIMARY KEY,
     device_id VARCHAR(100) NOT NULL,
-    temperature DOUBLE PRECISION, -- double precision för högre precision
-    humidity DOUBLE PRECISION,
-    vibration DOUBLE PRECISION,
-    fan_state VARCHAR(20),
+
+    temperature NUMERIC(5,2),
+    humidity NUMERIC(5,2),
+    fan_speed_setting INTEGER,
+    fan_speed_rpm INTEGER,
+    fan_status VARCHAR(20),
+    health_status VARCHAR(20),
+
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
-    FOREIGN KEY (device_id)
-        REFERENCES devices(device_id)
+    FOREIGN KEY (device_id) REFERENCES devices(device_id)
 );
 
 CREATE TABLE alerts (
     id BIGSERIAL PRIMARY KEY,
     device_id VARCHAR(100) NOT NULL,
-    severity VARCHAR(100) NOT NULL,
+    severity VARCHAR(20) NOT NULL,
     reason TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     FOREIGN KEY (device_id)
         REFERENCES devices(device_id)
-)
-
-
---{
---  "device_id": "vent-001",
---  "timestamp": "2026-09-03T10:30:00Z",
---  "temperature": 22.4,
---  "humidity": 45.8,
---  "fan_speed_setting": 72,
---  "fan_speed_rpm": 1450, (denna om sacharias hittar sin fläkt)
---  "fan_status": "running"
---  "health_status": "HEALTHY
---}
-
-
---Kör såhär
---temperature NUMERIC(5,2),
---humidity NUMERIC(5,2),
---fan_speed_setting INTEGER,
---fan_speed_rpm INTEGER,
---fan_status VARCHAR(20),
---health_status VARCHAR(20)
+);
