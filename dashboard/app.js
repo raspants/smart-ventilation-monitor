27040c85b292fafd4aa6e16a9331a84eb4dcb1a0
Your Name <you@example.com>
2026-09-07 09:07:26 +0200

    feat(dashboard): add initial ventilation dashboard

---
+++ dashboard/app.js
@@ -0,0 +1,71 @@
+const unitList = document.querySelector("section:first-child ul");
+const selectedUnit = document.querySelector("#selected-unit");
+
+
+
+fetch("http://localhost:5001/devices")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(device => {
+            console.log(device.device_id);
+            
+            const unit = document.createElement("li");
+            unit.textContent = device.device_id;
+            unit.addEventListener("click", () => {
+            selectedUnit.textContent = device.device_id;
+            });
+            
+            const health = document.createElement("span")
+
+            let emoji;
+            let fanText;
+
+            switch (device.status) {
+                case "HEALTHY":
+                    emoji = "🟢";
+                    break;
+                case "WARNING":
+                    emoji = "🟡";
+                    break;
+                case "CRITICAL":
+                    emoji = "🔴";
+                    break;
+                case "OFFLINE":
+                    emoji = "⚫";
+                    break;
+            }
+            
+            health.textContent = `${emoji} ${device.status}`;
+
+            unit.appendChild(health);
+
+            fetch(`http://localhost:5001/devices/${device.device_id}/latest`)
+                .then(response => response.json())
+                .then(measurement => {
+                    console.log(measurement);
+
+                    const fanStatus = document.createElement("span");
+    
+                    let emoji;
+
+                    switch (measurement.fan_status) {
+                        case "running":
+                            emoji = "🟢";
+                            fanText = "Running";
+                            break;
+                        case "offline":
+                            emoji = "⚫";
+                            fanText = "Offline";
+                            break;
+                    }
+                    
+                    fanStatus.textContent = `${emoji} ${fanText}`;
+
+                    unit.appendChild(fanStatus);
+                });
+
+            unitList.appendChild(unit);
+
+            
+        });
+    });
