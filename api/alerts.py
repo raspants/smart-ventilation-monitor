0c5f01c3cce054fb889b51934509439a1e6199a0
Your Name <you@example.com>
2026-09-09 16:37:29 +0200

    feat(api): add critical fan alert

--- api/alerts.py
+++ api/alerts.py
@@ -1,11 +1,11 @@
 def detect_alert(data, settings):
     alerts = []
 
-    if data["health_status"] == "CRITICAL":
+    if data["fan_status"] == "running" and data["fan_speed_rpm"] == 0:
         alerts.append({
             "device_id": data["device_id"],
             "severity": "CRITICAL",
-            "reason": f"Fan is running but no RPM is {data['fan_speed_rpm']}",
+            "reason": f"Fan is running but RPM is {data['fan_speed_rpm']}"
 
     })
  
