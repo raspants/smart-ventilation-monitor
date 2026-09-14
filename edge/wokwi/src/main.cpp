84810bc03286070a977ba1f99f808e454b1aedae
Your Name <you@example.com>
2026-09-14 10:37:34 +0200

    feat(edge): initialize esp32 wifi

--- edge/wokwi/src/main.cpp
+++ edge/wokwi/src/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
 extern "C" void app_main()
 {
-    printf("Smart_Vent started\n");
+    printf("SmartVent ESP32 started\n");
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    printf("Wi-Fi initialized\n");
 }
