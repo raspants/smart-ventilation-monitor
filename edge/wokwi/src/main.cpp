b5f88ef8c4da314a3aae2a384c0d4723d12c9d94
Your Name <you@example.com>
2026-09-14 10:51:18 +0200

    edge comunication handover

--- edge/wokwi/src/main.cpp
+++ edge/wokwi/src/main.cpp
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "freertos/task.h"
-#include "esp_wifi.h"
+
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
 #include "nvs_flash.h"
+
+#define WIFI_SSID "Wokwi-GUEST"
+
+static const char *TAG = "SmartVent";
+
+static void wifi_event_handler(
+    void *arg,
+    esp_event_base_t event_base,
+    int32_t event_id,
+    void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        ESP_LOGI(TAG, "Wi-Fi disconnected, retrying...");
+        esp_wifi_connect();
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Got IP address: " IPSTR, IP2STR(&event->ip_info.ip));
+    }
+}
 
 extern "C" void app_main()
 {
@@ -20,5 +49,36 @@ extern "C" void app_main()
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    printf("Wi-Fi initialized\n");
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(
+            WIFI_EVENT,
+            ESP_EVENT_ANY_ID,
+            &wifi_event_handler,
+            nullptr
+        )
+    );
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(
+            IP_EVENT,
+            IP_EVENT_STA_GOT_IP,
+            &wifi_event_handler,
+            nullptr
+        )
+    );
+
+    wifi_config_t wifi_config = {};
+
+    snprintf(
+        (char *)wifi_config.sta.ssid,
+        sizeof(wifi_config.sta.ssid),
+        "%s",
+        WIFI_SSID
+    );
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    printf("Connecting to Wi-Fi...\n");
 }
